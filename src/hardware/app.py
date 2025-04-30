6616a0825ab0a5cc5602ff50b57f3e2d82345e61
Bartosz Kurkus <bartejos@gmail.com>
2025-04-30 15:29:44 +0200

    Add motion sensor funcionality

--- src/hardware/app.py
+++ src/hardware/app.py
@@ -1,32 +1,36 @@
 import time
 import grovepi # type: ignore
 
-# Connect the Grove Light Sensor to analog port A0
-light_sensor = 0
-
-# Connect the LED to digital port D4
-led = 4
+# Ports 
+light_sensor = 0    # A0
+led = 4             # D4
+pir_sensor = 7      # D7
 
 # Turn on LED once sensor exceeds threshold resistance
 threshold = 10
 
 grovepi.pinMode(light_sensor,"INPUT")
 grovepi.pinMode(led,"OUTPUT")
+grovepi.pinMode(pir_sensor,"INPUT")
+
+def is_dark(sensor_value):
+    try:
+        resistance = (float)(1023 - sensor_value) * 10 / sensor_value  # kΩ
+    except ZeroDivisionError:
+        resistance = float('inf')  # Resistance is infinite if sensor_value is 0
+    return resistance > threshold, resistance
 
 while True:
     try:
         # Get sensor value
         sensor_value = grovepi.analogRead(light_sensor)
+        dark, resistance = is_dark(sensor_value)
 
-        # Calculate resistance of sensor in K
-        resistance = (float)(1023 - sensor_value) * 10 / sensor_value
-
-        if resistance > threshold:
-            # Send HIGH to switch on LED
-            grovepi.digitalWrite(led,1)
+        if dark:
+            if grovepi.digitalRead(pir_sensor):
+                grovepi.digitalWrite(led, 1)
         else:
-            # Send LOW to switch off LED
-            grovepi.digitalWrite(led,0)
+            grovepi.digitalWrite(led, 0)
 
         print("sensor_value = %d resistance = %.2f" %(sensor_value,  resistance))
         time.sleep(.5)
