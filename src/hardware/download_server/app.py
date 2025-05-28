b7822f13e97a2ffb6a91de2b9f48b50f12946549
Bartosz Kurkus <bartejos@gmail.com>
2025-05-28 03:48:59 +0200

    Integrate Tuya outlet control

--- src/hardware/download_server/app.py
+++ src/hardware/download_server/app.py
@@ -1,15 +1,27 @@
 from flask import Flask, request, Response
 import requests
 
-POST_URL = "http://sensor_server:5000/led"
+LED_URL = "http://localhost:5000/led"
+OUTLET_URL = "http://localhost:5000/outlet"
 
 app = Flask(__name__)
 
-@app.route('/forward', methods=['POST'])
-def forward_request():
+@app.route('/led', methods=['POST'])
+def forward_led():
     data = request.get_json()
     try:
-        response = requests.post(POST_URL, json=data)
+        response = requests.post(LED_URL, json=data)
+        return Response(response.content,
+                        status=response.status_code,
+                        content_type=response.headers.get('Content-Type', 'application/json'))
+    except Exception as e:
+        return {"status": "error", "message": str(e)}, 500
+
+@app.route('/outlet', methods=['POST'])
+def forward_outlet():
+    data = request.get_json()
+    try:
+        response = requests.post(OUTLET_URL, json=data)
         return Response(response.content,
                         status=response.status_code,
                         content_type=response.headers.get('Content-Type', 'application/json'))
@@ -17,4 +29,4 @@ def forward_request():
         return {"status": "error", "message": str(e)}, 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5002)
